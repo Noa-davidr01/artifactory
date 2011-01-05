1d0c520cced0bd1b151a1ca04a3ef109d0b158a6
Craig S. Cottingham <craig.cottingham@gmail.com>
2011-01-04 19:56:12 -0600

    Added basic specs for all API endpoints

--- spec/artifactory_spec.rb
+++ spec/artifactory_spec.rb
@@ -1,4 +1,177 @@
-describe "a first spec" do
-  it "should be successful" do
+describe "artifacts and storage" do
+  
+  it "gets folder info" do
   end
+  
+  it "gets file info" do
+  end
+  
+  it "gets item metadata names" do
+  end
+  
+  it "gets single item metadatum" do
+  end
+  
+  it "gets item metadata info" do
+  end
+  
+  it "deletes single item metadatum" do
+  end
+  
+  it "saves single item metadatum" do
+  end
+  
+  it "gets item properties info" do
+  end
+  
+  it "saves item properties" do
+  end
+  
+  it "retrieves an artifact" do
+  end
+  
+  it "deploys an artifact" do
+  end
+  
+  it "deletes an item" do
+  end
+  
+  it "copies an item" do
+  end
+  
+  it "moves an item" do
+  end
+  
+  it "replicates/downloads an artifact" do
+  end
+  
+  it "synchronizes/replicates a folder" do
+  end
+  
+  it "gets file list" do
+  end
+  
+  it "gets last modified time for an item" do
+  end
+  
+end
+
+describe "builds" do
+  
+  it "gets all builds" do
+  end
+
+  it "gets all runs for a given build" do
+  end
+
+  it "gets info for a given build" do
+  end
+
+  it "promotes a build via move" do
+  end
+
+  it "promotes a build via copy" do
+  end
+
+  it "deletes a build" do
+  end
+
+  it "renames a build" do
+  end
+  
+end
+
+describe "imports" do
+  
+  it "imports repository content" do
+  end
+  
+end
+
+describe "plugins" do
+  
+  it "executes plugin code" do
+  end
+  
+end
+
+describe "repositories" do
+  
+  it "gets a repositories details list" do
+  end
+  
+  it "retrieves repository configuration" do
+  end
+  
+  it "creates/replaces repository configuration" do
+  end
+  
+  it "updates repository configuration" do
+  end
+  
+  it "deletes a repository" do
+  end
+  
+  it "configures a remote repository" do
+  end
+  
+end
+
+describe "searches" do
+  
+  it "performs an artifact search (quick search)" do
+  end
+  
+  it "performs an archive entry search (class search)" do
+  end
+  
+  it "performs a GAVC search" do
+  end
+  
+  it "performs a property search" do
+  end
+  
+  it "performs an XPath search" do
+  end
+  
+  it "performs a checksum search" do
+  end
+  
+  it "searches for all artifacts not downloaded since a given date" do
+  end
+  
+  it "searches for all artifacts created within a date range" do
+  end
+  
+  it "performs a pattern search" do
+  end
+  
+  it "performs a license search" do
+  end
+  
+end
+
+describe "system and configuration" do
+  
+  it "gets system info" do
+  end
+  
+  it "pings for system health" do
+  end
+  
+  it "retrieves general configuration" do
+  end
+  
+  it "saves general configuration" do
+  end
+  
+  it "retrieves security configuration" do
+  end
+  
+  it "saves security configuration" do
+  end
+  
+  it "gets information about version and add-ons" do
+  end
+  
 end
